698dbebf6feeb76f171c8b3e583c21112c7355cc
lunaerem <140562305+lunaerem@users.noreply.github.com>
2026-05-14 19:05:59 -0500

    Update the game listing

--- src/components/games/KH.vue
+++ src/components/games/KH.vue
@@ -1,37 +1,40 @@
 <script setup lang="ts">
-import { inject, onMounted } from 'vue';
+import { inject, onMounted } from "vue";
 
-const toggleModal = inject('toggle') as any;
-const splash = inject('splash') as any;
+const toggleModal = inject("toggle") as any;
+const splash = inject("splash") as any;
 
 onMounted(() => {
-    splash.value = document.querySelector('.splash-img');
-})
+  splash.value = document.querySelector(".splash-img");
+});
 </script>
 
 <template>
-    <div id="timeline-splash" class="splash-img">
-	<h1>Timeline Template</h1>
+  <div id="timeline-splash" class="splash-img">
+    <h1>
+      Kingdom Hearts <br />
+      Series Timeline
+    </h1>
+  </div>
+  <section>
+    <div class="set-ratio">
+      <h2>Description</h2>
+      <p>Placeholder</p>
     </div>
-    <section>
-	<div class="set-ratio">
-	    <h2>Description</h2>
-	    <p>Placeholder</p>
-	</div>
-    </section>
-    <section>
-	<div class="set-ratio">
-	    <h2>Timeline</h2>
-	</div>
-    </section>
-    <div>
-	Just here for testing purposes, remove later.
-	<button @click="toggleModal('Game2')">Popup Test</button>
+  </section>
+  <section>
+    <div class="set-ratio">
+      <h2>Timeline</h2>
     </div>
+  </section>
+  <div>
+    Just here for testing purposes, remove later.
+    <button @click="toggleModal('Game2')">Popup Test</button>
+  </div>
 </template>
 
 <style scoped>
 #timeline-splash {
-    background-image: url("../../assets/splash/kh.svg");
+  background-image: url("../../assets/splash/kh.svg");
 }
 </style>
